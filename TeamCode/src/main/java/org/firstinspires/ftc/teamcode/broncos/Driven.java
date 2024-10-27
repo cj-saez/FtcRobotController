7d7ee5d69aff8ac08f3af33ca3a64d9d57144b52
IKnowJava1234 <144849816+IKnowJava1234@users.noreply.github.com>
2024-10-27 12:01:34 -0500

    Update Driven.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/broncos/Driven.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/broncos/Driven.java
@@ -13,6 +13,8 @@ public class Driven extends OpMode
     DcMotor backLeft;
     DcMotor backRight;
     DcMotor slideDrive;
+    DcMotor armRotation;
+    DcMotor armExtension;
 
 
     @Override
@@ -22,6 +24,8 @@ public class Driven extends OpMode
         this.backLeft = hardwareMap.get(DcMotor.class, "bl");
         this.backRight = hardwareMap.get(DcMotor.class, "br");
         this.slideDrive = hardwareMap.get(DcMotor.class, "Slide");
+        this.armRotation = hardwareMap.get(DcMotor.class, "arm");
+        this.armExtension = hardwareMap.get(DcMotor.class, "extension");
 
 // whats up ????????
         //#wecandothis
@@ -37,19 +41,24 @@ public class Driven extends OpMode
         double strafe = -gamepad1.left_stick_x;
         double turn = gamepad1.right_stick_x;
         double slideDrive = gamepad1.right_stick_y;
+        double armRotation = gamepad1.left_trigger;
+        double armExtension = gamepad1.right_trigger;
 
         double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(turn), .45);
         double frontLeftPower = (drive + strafe + turn) / denominator;
         double frontRightPower = (drive - strafe - turn) / denominator;
         double backLeftPower = (drive - strafe + turn) / denominator;
         double backRightPower = (drive + strafe - turn) / denominator;
+        double slideDrivePower = slideDrive;
 
+        
         frontLeft.setPower(frontLeftPower);
         frontRight.setPower(frontRightPower);
         backLeft.setPower(backLeftPower);
         backRight.setPower(backRightPower);
-
-        slideDrive.setPower(slideDrive);
+        this.slideDrive.setPower(slideDrivePower);
+        this.armRotation.setPower(1);
+        this.armExtension.setPower(1);
 
 //
 //
